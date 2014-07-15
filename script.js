065a73422df201e67c07984745972dc33ce56b60
Ipalibo <ipalibowhyte@gmail.com>
2014-07-15 17:46:35 +0100

    Added navbar panel also added animation when the page loads

--- script.js
+++ script.js
@@ -1,9 +1,69 @@
+var activeLink = "All";
+
 $( document ).ready(function() {
 
-$(':radio').change(
-  function(){
-    $('.choice').text( this.value + ' stars' );
-  } 
-);
+	$( "#mainBody" ).animate({opacity: "1"}, 500, function(){
+		$( ".main" ).animate({left: "240px"}, 800, function(){
 
+			$( ".navBar" ).animate({bottom: "0px"}, 500);
+		});
+	});
+
+	$("#all").bind("click", function(){
+
+		if(activeLink != "All"){
+			activeLink = ($(this).text());
+			  $( "#nav_indicator" ).animate({
+			    left: "0%"
+			  }, 700, function() {
+			    // Animation complete.
+			  });
+
+		}else{
+			
+		}
+	});
+
+	$("#easy").bind("click", function(){
+
+		if(activeLink != "Easy"){
+			activeLink=($(this).text());
+			$( "#nav_indicator" ).animate({
+			    left: "25%"
+			  }, 700, function() {
+			    // Animation complete.
+			 });
+		}else{
+
+		}
+	});
+
+	$("#medium").bind("click", function(){
+
+		if(activeLink != "Medium"){
+				activeLink=($(this).text());
+				$( "#nav_indicator" ).animate({
+				    left: "50%"
+				  }, 700, function() {
+				    // Animation complete.
+				 });
+			}else{
+
+			}
+	});
+
+
+	$("#difficult").bind("click", function(){
+
+		if(activeLink != "Difficult"){
+				activeLink=($(this).text());
+				$( "#nav_indicator" ).animate({
+				    left: "75%"
+				  }, 700, function() {
+				    // Animation complete.
+				 });
+			}else{
+
+			}
+	});
 });
