6d3ba050d0861312f2074dc61c8be064caab092d
Ipalibo <ipalibowhyte@gmail.com>
2014-07-17 01:53:16 +0100

    fixed layout added meal type section

--- script.js
+++ script.js
@@ -1,22 +1,130 @@
-var activeLink = "All";
+var activeLink = "";
+const MAIN_HEIGHT = "20%";
+const MAIN_WIDTH = "20%"
 
 $( document ).ready(function() {
+var phonecatApp = angular.module('phonecatApp', []);
 
-	$( "#mainBody" ).animate({opacity: "1"}, 500, function(){
-		$( ".main" ).animate({left: "240px"}, 800, function(){
+	phonecatApp.controller('PhoneListCtrl', function ($scope) {
+  $scope.reciepes = 
+  [
+    {
+	  "type": "Breakfast",
+	  "title": "Chili con carne",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 4,
+	  "ingredients": 
+		  [
+		    {
+		      "vegetable": "40ml"
+		    }
+		  ],
+	  "method": 
+		  [
+		    {
+		      "1": "In a medium sized stock pot, heat the oil over medium heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+		  ]
+	},
 
-			$( ".navBar" ).animate({bottom: "0px"}, 500);
+	{
+	  "type": "Main Meal",
+	  "title": "Main Meal",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 4,
+	  "ingredients": 
+	  	[
+		    {
+		      "vegetable": "40ml"
+		    }
+	  	],
+	  "method": 
+	  	[
+
+		    {
+		      "1": "In a medium sized stock pot, heat the oil over medium heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+	  	]
+	},
+
+	{
+	  "type": "Dessert",
+	  "title": "dessert",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 4,
+	  "ingredients": 
+	  	[
+		    {
+		      "vegetable": "40ml"
+		    }
+	  	],
+	  "method": 
+	  	[
+
+		    {
+		      "1": "In a medium sized stock pot, heat the oil over medium heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+	  	]
+	},
+
+	{
+	  "type": "Side dish/snack",
+	  "title": "",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 4,
+	  "ingredients": 
+	  	[
+		    {
+		      "vegetable": "40ml"
+		    }
+	  	],
+	  "method": 
+	  	[
+
+		    {
+		      "1": "In a medium sized stock pot, heat the oil over medium heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+	  	]
+	},
+
+	{
+	  "type": "Beverage",
+	  "title": "Main Meal",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 4,
+	  "ingredients": 
+	  	[
+		    {
+		      "vegetable": "40ml"
+		    }
+	  	],
+	  "method": 
+	  	[
+
+		    {
+		      "1": "In a medium sized stock pot, heat the oil over medium heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+	  	]
+	}
+
+  ];
+});
+
+	initialiseWithall();
+
+	$( "#mainBody" ).animate({opacity: "1"}, 600, function(){
+		$( ".navBar" ).animate({bottom: "10px"}, 300, function(){
+			$( ".navBar" ).animate({bottom: "-1px"}, 200);
 		});
 	});
 
 	$("#all").bind("click", function(){
 
-		if(activeLink != "All"){
+		if(activeLink != "all"){
+			restoreOriginalfont();
 			activeLink = ($(this).text());
-			  $( "#nav_indicator" ).animate({
-			    left: "0%"
-			  }, 700, function() {
-			    // Animation complete.
+			  $( "#nav_indicator" ).animate({left: "0%",}, 500, function() {
+			    $("#all").animate({"color":"#efbe5c","font-size":"14pt"}, 100);
 			  });
 
 		}else{
@@ -27,11 +135,11 @@ $( document ).ready(function() {
 	$("#easy").bind("click", function(){
 
 		if(activeLink != "Easy"){
+			restoreOriginalfont();
 			activeLink=($(this).text());
-			$( "#nav_indicator" ).animate({
-			    left: "25%"
-			  }, 700, function() {
+			$( "#nav_indicator" ).animate({left: "25%"}, 500, function() {
 			    // Animation complete.
+			    $("#easy").animate({"color":"#efbe5c","font-size":"14pt"}, 100);
 			 });
 		}else{
 
@@ -39,31 +147,42 @@ $( document ).ready(function() {
 	});
 
 	$("#medium").bind("click", function(){
-
 		if(activeLink != "Medium"){
-				activeLink=($(this).text());
-				$( "#nav_indicator" ).animate({
-				    left: "50%"
-				  }, 700, function() {
-				    // Animation complete.
-				 });
-			}else{
+			restoreOriginalfont();
+			activeLink=($(this).text());
+			$( "#nav_indicator" ).animate({left: "50%"}, 500, function() {
+			    // Animation complete.
+			    $("#medium").animate({"color":"#efbe5c","font-size":"14pt"}, 100);
+			 });
+		}else{
 
-			}
+		}
 	});
-
 
 	$("#difficult").bind("click", function(){
 
 		if(activeLink != "Difficult"){
-				activeLink=($(this).text());
-				$( "#nav_indicator" ).animate({
-				    left: "75%"
-				  }, 700, function() {
-				    // Animation complete.
-				 });
-			}else{
+			restoreOriginalfont();			
+			activeLink=($(this).text());
+			$( "#nav_indicator" ).animate({
+			    left: "75%"
+			  }, 500, function() {
+			    // Animation complete.
+			    $("#difficult").animate({"color":"#efbe5c","font-size":"14pt"}, 100);				    
+			 });
+		}else{
 
-			}
+		}
 	});
 });
+
+function restoreOriginalfont(){
+	$("#"+activeLink.toLowerCase()).animate({"color":"#efbe5c","font-size":"13px"}, 100);
+}
+
+function initialiseWithall(){
+	$('#all').find('a').trigger('click');
+
+}
+
+
