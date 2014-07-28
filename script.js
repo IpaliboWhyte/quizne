355e7a30cee97d3125a6cdcfcf19321f3c2ee671
Ipalibo <ipalibowhyte@gmail.com>
2014-07-28 12:48:04 +0100

    added pagination to handle lots of data

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var activeLink = "";
 const MAIN_HEIGHT = "20%";
 const MAIN_WIDTH = "20%"
+var original = $('.navBar');
 
 $( document ).ready(function() {
 var phonecatApp = angular.module('phonecatApp', []);
@@ -11,6 +12,21 @@ angular.module('mealApp', [])
     $scope.recipes = mealFactory.recipes;
     $scope.selectedMeals = [];
     $scope.text = '';
+
+    var pagesShown = 1;
+	var pageSize = 8;
+		    
+    $scope.paginationLimit = function(data) {
+        return pageSize * pagesShown;
+    };
+
+    $scope.hasMoreItemsToShow = function() {
+		return pagesShown < ($scope.recipes.length / pageSize);
+	};
+
+	$scope.showMoreItems = function() {
+        pagesShown = pagesShown + 1;       
+    };
 
     $scope.selectMeal = function(meal) {
   		$scope.selectedMeal = meal;
@@ -256,6 +272,546 @@ angular.module('mealApp', [])
 		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
 		    }
 	  	]
+	},
+
+	 {
+	  "type": "Breakfast",
+	  "title": "Chili con carne",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 4,
+	  "duration": 12,
+	  "level":"medium",
+	  "url":"http://31.media.tumblr.com/bc0ea7c5f95701bff499f78b59d23e68/tumblr_mr74z9Lt3O1rs0z5go1_500.jpg",
+	  "ingredients": 
+		  [
+		    {
+		      "vegetable": "40ml"
+		    }
+		  ],
+	  "method": 
+		  [
+		    {
+		      "1": "In a medium sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+		  ]
+	},
+
+	{
+	  "type": "Breakfast",
+	  "title": "Spicy Indomie",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 5,
+	  "duration": 30,
+	  "level":"hot",
+	  "url":"http://38.media.tumblr.com/875b5eeb5b1efa37d2e9d36fbad836d3/tumblr_mzczesVrZD1rimr6yo1_1280.jpg",
+	  "ingredients": 
+		  [
+		    {
+		      "vegetable": "40ml"
+		    }
+		  ],
+	  "method": 
+		  [
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+		  ]
+	},
+
+	{
+	  "type": "Breakfast",
+	  "title": "Prawn Vegs",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 4,
+	  "duration": 25,
+	  "level":"medium",
+	  "url":"http://38.media.tumblr.com/23f5902bec9d2f6f600043a7fb594a6b/tumblr_n8ykd5t78n1rzwv55o1_500.png",
+	  "ingredients": 
+		  [
+		    {
+		      "vegetable": "40ml"
+		    }
+		  ],
+	  "method": 
+		  [
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+		  ]
+	},
+
+	{
+	  "type": "Main Meal",
+	  "title": "skewer",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 2,
+	  "duration": 20,
+	  "level":"hot",
+	  "url":"http://31.media.tumblr.com/tumblr_m1t5wqIm9g1qbih6so1_500.jpg",
+	  "ingredients": 
+		  [
+		    {
+		      "vegetable": "40ml"
+		    }
+		  ],
+	  "method": 
+		  [
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+		  ]
+	},
+
+	{
+	  "type": "Main Meal",
+	  "title": "Vegetable bhaji salad",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 4,
+	  "duration": 15,
+	  "level":"hot",
+	  "url":"http://s9.favim.com/orig/130818/-_-food-pancake-Favim.com-858765.png",
+	  "ingredients": 
+	  	[
+		    {
+		      "vegetable": "40ml"
+		    }
+	  	],
+	  "method": 
+	  	[
+
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+	  	]
+	},
+
+	{
+	  "type": "Dessert",
+	  "title": "Caramel fudge",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 1,
+	  "duration": 50,
+	  "level":"medium",
+	  "url":"http://31.media.tumblr.com/246db30925693c42cc88e2e3b4cd8f39/tumblr_n3rn31akff1qzk4ruo1_500.jpg",
+	  "ingredients": 
+	  	[
+		    {
+		      "vegetable": "40ml"
+		    }
+	  	],
+	  "method": 
+	  	[
+
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+	  	]
+	},
+
+	{
+	  "type": "Side dish/snack",
+	  "title": "Mixed veg",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 5,
+	  "duration": 5,
+	  "level":"sweet",
+	  "url":"http://33.media.tumblr.com/29cee3f9474c8d8de183f5027bb35a06/tumblr_ms11l8E3Ml1rzwv55o1_500.png",
+	  "ingredients": 
+	  	[
+		    {
+		      "vegetable": "40ml"
+		    }
+	  	],
+	  "method": 
+	  	[
+
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+	  	]
+	},
+
+	{
+	  "type": "Beverage",
+	  "title": "Spicy HamBurger",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 4,
+	  "duration": 32,
+	  "level":"hot",
+	  "url":"http://37.media.tumblr.com/51bedc9e7334de555c1b00dd3ed8cf22/tumblr_mo9e0eI0fa1riu320o1_500.jpg",
+	  "ingredients": 
+	  	[
+		    {
+		      "vegetable": "40ml"
+		    }
+	  	],
+	  "method": 
+	  	[
+
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+	  	]
+	},
+
+	 {
+	  "type": "Breakfast",
+	  "title": "Chili con carne",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 4,
+	  "duration": 12,
+	  "level":"medium",
+	  "url":"http://31.media.tumblr.com/bc0ea7c5f95701bff499f78b59d23e68/tumblr_mr74z9Lt3O1rs0z5go1_500.jpg",
+	  "ingredients": 
+		  [
+		    {
+		      "vegetable": "40ml"
+		    }
+		  ],
+	  "method": 
+		  [
+		    {
+		      "1": "In a medium sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+		  ]
+	},
+
+	{
+	  "type": "Breakfast",
+	  "title": "Spicy Indomie",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 5,
+	  "duration": 30,
+	  "level":"hot",
+	  "url":"http://38.media.tumblr.com/875b5eeb5b1efa37d2e9d36fbad836d3/tumblr_mzczesVrZD1rimr6yo1_1280.jpg",
+	  "ingredients": 
+		  [
+		    {
+		      "vegetable": "40ml"
+		    }
+		  ],
+	  "method": 
+		  [
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+		  ]
+	},
+
+	{
+	  "type": "Breakfast",
+	  "title": "Prawn Vegs",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 4,
+	  "duration": 25,
+	  "level":"medium",
+	  "url":"http://38.media.tumblr.com/23f5902bec9d2f6f600043a7fb594a6b/tumblr_n8ykd5t78n1rzwv55o1_500.png",
+	  "ingredients": 
+		  [
+		    {
+		      "vegetable": "40ml"
+		    }
+		  ],
+	  "method": 
+		  [
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+		  ]
+	},
+
+	{
+	  "type": "Main Meal",
+	  "title": "skewer",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 2,
+	  "duration": 20,
+	  "level":"hot",
+	  "url":"http://31.media.tumblr.com/tumblr_m1t5wqIm9g1qbih6so1_500.jpg",
+	  "ingredients": 
+		  [
+		    {
+		      "vegetable": "40ml"
+		    }
+		  ],
+	  "method": 
+		  [
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+		  ]
+	},
+
+	{
+	  "type": "Main Meal",
+	  "title": "Vegetable bhaji salad",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 4,
+	  "duration": 15,
+	  "level":"hot",
+	  "url":"http://s9.favim.com/orig/130818/-_-food-pancake-Favim.com-858765.png",
+	  "ingredients": 
+	  	[
+		    {
+		      "vegetable": "40ml"
+		    }
+	  	],
+	  "method": 
+	  	[
+
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+	  	]
+	},
+
+	{
+	  "type": "Dessert",
+	  "title": "Caramel fudge",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 1,
+	  "duration": 50,
+	  "level":"medium",
+	  "url":"http://31.media.tumblr.com/246db30925693c42cc88e2e3b4cd8f39/tumblr_n3rn31akff1qzk4ruo1_500.jpg",
+	  "ingredients": 
+	  	[
+		    {
+		      "vegetable": "40ml"
+		    }
+	  	],
+	  "method": 
+	  	[
+
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+	  	]
+	},
+
+	{
+	  "type": "Side dish/snack",
+	  "title": "Mixed veg",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 5,
+	  "duration": 5,
+	  "level":"sweet",
+	  "url":"http://33.media.tumblr.com/29cee3f9474c8d8de183f5027bb35a06/tumblr_ms11l8E3Ml1rzwv55o1_500.png",
+	  "ingredients": 
+	  	[
+		    {
+		      "vegetable": "40ml"
+		    }
+	  	],
+	  "method": 
+	  	[
+
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+	  	]
+	},
+
+	{
+	  "type": "Beverage",
+	  "title": "Spicy HamBurger",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 4,
+	  "duration": 32,
+	  "level":"hot",
+	  "url":"http://37.media.tumblr.com/51bedc9e7334de555c1b00dd3ed8cf22/tumblr_mo9e0eI0fa1riu320o1_500.jpg",
+	  "ingredients": 
+	  	[
+		    {
+		      "vegetable": "40ml"
+		    }
+	  	],
+	  "method": 
+	  	[
+
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+	  	]
+	},
+
+	 {
+	  "type": "Breakfast",
+	  "title": "Chili con carne",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 4,
+	  "duration": 12,
+	  "level":"medium",
+	  "url":"http://31.media.tumblr.com/bc0ea7c5f95701bff499f78b59d23e68/tumblr_mr74z9Lt3O1rs0z5go1_500.jpg",
+	  "ingredients": 
+		  [
+		    {
+		      "vegetable": "40ml"
+		    }
+		  ],
+	  "method": 
+		  [
+		    {
+		      "1": "In a medium sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+		  ]
+	},
+
+	{
+	  "type": "Breakfast",
+	  "title": "Spicy Indomie",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 5,
+	  "duration": 30,
+	  "level":"hot",
+	  "url":"http://38.media.tumblr.com/875b5eeb5b1efa37d2e9d36fbad836d3/tumblr_mzczesVrZD1rimr6yo1_1280.jpg",
+	  "ingredients": 
+		  [
+		    {
+		      "vegetable": "40ml"
+		    }
+		  ],
+	  "method": 
+		  [
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+		  ]
+	},
+
+	{
+	  "type": "Breakfast",
+	  "title": "Prawn Vegs",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 4,
+	  "duration": 25,
+	  "level":"medium",
+	  "url":"http://38.media.tumblr.com/23f5902bec9d2f6f600043a7fb594a6b/tumblr_n8ykd5t78n1rzwv55o1_500.png",
+	  "ingredients": 
+		  [
+		    {
+		      "vegetable": "40ml"
+		    }
+		  ],
+	  "method": 
+		  [
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+		  ]
+	},
+
+	{
+	  "type": "Main Meal",
+	  "title": "skewer",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 2,
+	  "duration": 20,
+	  "level":"hot",
+	  "url":"http://31.media.tumblr.com/tumblr_m1t5wqIm9g1qbih6so1_500.jpg",
+	  "ingredients": 
+		  [
+		    {
+		      "vegetable": "40ml"
+		    }
+		  ],
+	  "method": 
+		  [
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+		  ]
+	},
+
+	{
+	  "type": "Main Meal",
+	  "title": "Vegetable bhaji salad",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 4,
+	  "duration": 15,
+	  "level":"hot",
+	  "url":"http://s9.favim.com/orig/130818/-_-food-pancake-Favim.com-858765.png",
+	  "ingredients": 
+	  	[
+		    {
+		      "vegetable": "40ml"
+		    }
+	  	],
+	  "method": 
+	  	[
+
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+	  	]
+	},
+
+	{
+	  "type": "Dessert",
+	  "title": "Caramel fudge",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 1,
+	  "duration": 50,
+	  "level":"medium",
+	  "url":"http://31.media.tumblr.com/246db30925693c42cc88e2e3b4cd8f39/tumblr_n3rn31akff1qzk4ruo1_500.jpg",
+	  "ingredients": 
+	  	[
+		    {
+		      "vegetable": "40ml"
+		    }
+	  	],
+	  "method": 
+	  	[
+
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+	  	]
+	},
+
+	{
+	  "type": "Side dish/snack",
+	  "title": "Mixed veg",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 5,
+	  "duration": 5,
+	  "level":"sweet",
+	  "url":"http://33.media.tumblr.com/29cee3f9474c8d8de183f5027bb35a06/tumblr_ms11l8E3Ml1rzwv55o1_500.png",
+	  "ingredients": 
+	  	[
+		    {
+		      "vegetable": "40ml"
+		    }
+	  	],
+	  "method": 
+	  	[
+
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+	  	]
+	},
+
+	{
+	  "type": "Beverage",
+	  "title": "Spicy HamBurger",
+	  "description": "A spicy and fragrant chili with ground beef, kidney beans, tomatoes, onions and garlic. Best served over rice with a dollop of sour cream and some cheese on top.",
+	  "ratings": 4,
+	  "duration": 32,
+	  "level":"hot",
+	  "url":"http://37.media.tumblr.com/51bedc9e7334de555c1b00dd3ed8cf22/tumblr_mo9e0eI0fa1riu320o1_500.jpg",
+	  "ingredients": 
+	  	[
+		    {
+		      "vegetable": "40ml"
+		    }
+	  	],
+	  "method": 
+	  	[
+
+		    {
+		      "1": "In a  sized stock pot, heat the oil over  heat. Saute onions, chile peppers andgarlic until soft."
+		    }
+	  	]
 	}
 
   ]
@@ -263,6 +819,7 @@ angular.module('mealApp', [])
 }]);
 
 	initialiseWithall();
+
 
 	$( "li.fade" ).hover(function() {
 	  	$( this ).fadeOut( 100 );
